ccc95866679cf6899addc5b96dcfbc10d4b1ad84
NaomiNyakaru <naomi.nyakaru@gmail.com>
2026-07-31 23:15:21 +0300

    Serve media files in production:update urls file )

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +14,6 @@ urlpatterns = [
     path('api/v1/admin/',   include('apps.adminpanel.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
